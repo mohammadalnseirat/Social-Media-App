51478630d2634de44f05792b7e899c3ef06da52c
mohammadalnseirat98 <mohammad.alnseirat@titanlabs.tech>
2025-03-24 21:04:33 +0400

    Implement Authentication Using Clerk with next JS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${manrope.className}`}>{children}</body>
+      <body className={`${manrope.className}`}>
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
     </html>
   );
 }
